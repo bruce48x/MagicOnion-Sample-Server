6c6dc6334656c070e2510cbe28f1e2e4031499e4
bruce <bruce48@foxmail.com>
2024-07-18 09:30:57 +0800

    init server

---
+++ Shared/Interfaces/IMyFirstService.cs
@@ -0,0 +1,9 @@
+﻿using MagicOnion;
+
+namespace Shared.Interfaces
+{
+    public interface IMyFirstService : IService<IMyFirstService>
+    {
+        UnaryResult<int> SumAsync(int x, int y);
+    }
+}
